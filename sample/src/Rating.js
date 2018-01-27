c3a8de8e741622da4dadf21b38bde26be0d18eaa
Praveen <xav.axp@gmail.com>
2018-01-27 12:25:15 +0530

    Added Rating component to the project

---
+++ sample/src/Rating.js
@@ -0,0 +1,24 @@
+import React,{Component} from 'react';
+import {Glyphicon} from 'react-bootstrap';
+
+class Rating extends Component{
+
+    render(){
+        return(
+            <div>
+                <h4>Rating: {this.props.rating}</h4>
+                <Glyphicon glyph={this.props.rating >= 1? "star":"star-empty"} />
+                <Glyphicon glyph={this.props.rating >= 2 ? "star" : "star-empty"} />
+                <Glyphicon glyph={this.props.rating >= 3 ? "star" : "star-empty"} />
+                <Glyphicon glyph={this.props.rating >= 4 ? "star" : "star-empty"} />
+                <Glyphicon glyph={this.props.rating >= 5 ? "star" : "star-empty"} />
+
+
+            </div>
+
+        );
+    }
+
+}
+
+export default Rating;
