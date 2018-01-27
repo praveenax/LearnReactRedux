8897adf6b060327469ada0f8afc5779f73841beb
Praveen <xav.axp@gmail.com>
2018-01-27 21:58:21 +0530

    added app instead of app2

--- sample/src/index.js
+++ sample/src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-// import App from './App';
-import App from './App2';
+import App from './App';
+// import App from './App2';
 
 import registerServiceWorker from './registerServiceWorker';
 
