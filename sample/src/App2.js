93c2aade52079e54a04403989ea36accef43e4b5
Praveen <xav.axp@gmail.com>
2018-01-27 11:26:29 +0530

    Added populate product in products component

--- sample/src/App2.js
+++ sample/src/App2.js
@@ -6,13 +6,23 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+
+    formatName(user){
+        return user.firstName + ' ' + user.lastName;
+    }
+
     render() {
+
+        const user = {
+            firstName:'Greg',
+            lastName:'Lim'
+        };
+
         return (
             <div className="App">
-                <h1>My First React App!</h1>
+                <h1>Hello, {this.formatName(user)}</h1>
                 <Products />
-                <Products />
-                <Products />
+                
                     
             </div>
         );
--- sample/src/Products.js
+++ sample/src/Products.js
@@ -1,12 +1,30 @@
 import React, {Component} from 'react';
 
 class Products extends Component{
+
+    populateProduct(product){
+    return <li key={product.toString()}>{product}</li>;
+    }
+
+    populateList(products){
+        // return products.map((product) => <li key={product.toString()}>{product}</li>);
+        return products.map((product) => this.populateProduct(product));
+        
+    }
+
     render(){
+
+        const products = ["Learning React","Pro React","Beginning React","Angular"];
+
+        const listProducts = this.populateList(products);
         return(
             <div>
                 <h2>
                     Products
                 </h2>
+                <ul>
+                    {listProducts}
+                </ul>
             </div>
         );
     }
