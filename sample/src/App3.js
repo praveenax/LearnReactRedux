488f98a6f9c7b60cd3e50eb0c922a05ae2f46e6d
Praveen <xav.axp@gmail.com>
2018-01-28 02:05:23 +0530

    Adding in new app view for showing git user

---
+++ sample/src/App3.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import './App.css';
+
+import Github from './component/Github';
+
+class App3 extends Component {
+
+    
+
+    render() {
+        return (
+            <div className="App">
+                <Github />
+            </div>
+        );
+    }
+}
+
+export default App3;
