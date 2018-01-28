9d0a79698e497182feacb5a6b1beacfbbab1376b
Praveen <xav.axp@gmail.com>
2018-01-28 15:15:24 +0530

    — Added a separate app component to test firebase database

---
+++ sample/src/App5.js
@@ -0,0 +1,30 @@
+import React, { Component } from 'react';
+import './App.css';
+
+// import * as firebase from 'firebase';
+
+// import Users from './component/Users';
+import Messages from './component/Messages';
+
+
+class App extends Component {
+
+    constructor(props){
+        super(props);
+        // console.log(firebase);
+    }
+
+    render() {
+        return (
+            <div className="App">
+               {/* <h2>Test</h2> */}
+               {/* <Users /> */}
+               <Messages />
+               
+            </div>
+        );
+    }
+}
+
+export default App;
+
