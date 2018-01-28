3e95936427590c507ff166cc066b2c824ccf5749
Praveen <xav.axp@gmail.com>
2018-01-28 10:36:21 +0530

    Added latest changes to App3 to include router redirection logic

--- sample/src/App3.js
+++ sample/src/App3.js
@@ -3,17 +3,74 @@ import './App.css';
 
 import Github from './component/Github';
 
-class App3 extends Component {
+import {BrowserRouter,Route,Switch} from 'react-router-dom';
 
-    
+class App3 extends Component {
 
     render() {
         return (
             <div className="App">
-                <Github />
+                <Header />
             </div>
         );
     }
 }
 
 export default App3;
+
+
+class Header extends Component{
+
+    render(){
+        return(
+
+            <BrowserRouter>
+            <div>
+                <Switch>
+                    <Route path="/github" component={Github} />
+                    <Route path="/" component={Home} />
+                    <Route path="/*" component={NotFound} />
+                    
+                </Switch>
+
+            </div>
+                </BrowserRouter>
+            
+
+        );
+    }
+
+}
+
+class Home extends Component {
+
+    render() {
+        return (
+
+            <div>
+                Home
+
+            </div>
+
+
+        );
+    }
+
+}
+
+class NotFound extends Component {
+
+    render() {
+        return (
+
+            <div>
+
+                Not Found
+
+            </div>
+
+
+        );
+    }
+
+}
