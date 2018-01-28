d51b3765eb1fac8c1b6f3e117b65a8b42d53af69
Praveen <xav.axp@gmail.com>
2018-01-28 11:18:47 +0530

    Added App4 to the project

--- note.md
+++ note.md
@@ -2,3 +2,8 @@ github:
 
 # https://api.github.com/search/users?q=<search term>
 
+
+TIps:
+
+1)Linting
+    
--- sample/src/index.js
+++ sample/src/index.js
@@ -3,7 +3,9 @@ import ReactDOM from 'react-dom';
 import './index.css';
 // import App from './App';
 // import App from './App2';
-import App from './App3';
+// import App from './App3';
+import App from './App4';
+
 
 
 import registerServiceWorker from './registerServiceWorker';
